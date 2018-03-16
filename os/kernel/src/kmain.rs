6cd5600a72f9bea5c9f6fca30641051be78d0ff5
Bill Liu <yuan6liu@gmail.com>
2018-03-16 00:16:27 -0700

    added files for assignment2

--- os/kernel/src/kmain.rs
+++ os/kernel/src/kmain.rs
@@ -6,20 +6,43 @@
 #![feature(decl_macro)]
 #![feature(repr_align)]
 #![feature(attr_literals)]
+
+#![feature(exclusive_range_pattern)]
+#![feature(alloc, allocator_api, global_allocator)]
+
 #![feature(never_type)]
 #![feature(ptr_internals)]
 #![feature(pointer_methods)]
 
+#[macro_use]
+#[allow(unused_imports)]
+extern crate alloc;
 extern crate pi;
 extern crate stack_vec;
+extern crate fat32;
 
+pub mod allocator;
 pub mod lang_items;
 pub mod mutex;
 pub mod console;
 pub mod shell;
+pub mod fs;
+
+#[cfg(not(test))]
+use allocator::Allocator;
+use fs::FileSystem;
+
+#[cfg(not(test))]
+#[global_allocator]
+pub static ALLOCATOR: Allocator = Allocator::uninitialized();
+
+pub static FILE_SYSTEM: FileSystem = FileSystem::uninitialized();
 
 #[no_mangle]
+#[cfg(not(test))]
 pub extern "C" fn kmain() {
+
+    // ALLOCATOR.initialize();
 
     // FIXME: Start the shell.
 
@@ -27,4 +50,5 @@ pub extern "C" fn kmain() {
     gpio_16_out.set();
 
     shell::shell( "~>" );
+
 }
